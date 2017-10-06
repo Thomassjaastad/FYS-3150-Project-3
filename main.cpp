bcb015560b4983360a72ce2d327903da11946421
Thomassjaastad <Thomassjaastad@users.noreply.github.com>
2017-10-06 12:30:33 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include "solarsystem.h"
+#include "euler.h"
+#include "velocityverlet.h"
+#include "vec3.h"
+using namespace std;
+
+int main(int numArguments, char **arguments)
+{    
+    int numTimesteps = 100;
+    if(numArguments >= 2) numTimesteps = atoi(arguments[1]);
+
+    SolarSystem solarSystem;
+    // We create new bodies like this. Note that the createCelestialBody function returns a reference to the newly created body
+    // This can then be used to modify properties or print properties of the body if desired
+    // Use with: solarSystem.createCelestialBody( position, velocity, mass );
+
+    solarSystem.createCelestialBody( vec3(0,0,0), vec3(0,0,0), 1.0 ); // Sun
+
+    // We don't need to store the reference, but just call the function without a left hand side
+    solarSystem.createCelestialBody( vec3(1, 0, 0), vec3(0, 2*M_PI, 0), 3e-6 ); // Earth
+    //solarSystem.createCelestialBody( vec3(0, 1, 0), vec3(2*M_PI, 0, 0), 3e-6 ); New planet
+
+    // To get a list (a reference, not copy) of all the bodies in the solar system, we use the .bodies() function
+    vector<CelestialBody> & bodies = solarSystem.bodies();
+
+    for(int i = 0; i < bodies.size(); i++) {
+        CelestialBody &body = bodies[i]; // Reference to this body
+        cout << "The position of this object is " << body.position << " with velocity " << body.velocity << endl;
+    }
+
+    double dt = 0.001;
+    Euler integrator(dt);
+    //VelocityVerlet integrator(dt);
+    solarSystem.calculateForcesAndEnergy();
+    for(int timestep=0; timestep<numTimesteps; timestep++) {
+        integrator.integrateOneStep(solarSystem);
+        solarSystem.writeToFile("positions.txt");
+    }
+
+    cout << "I just created my first solar system that has " << solarSystem.bodies().size() << " objects." << endl;
+    return 0;
+}
+
